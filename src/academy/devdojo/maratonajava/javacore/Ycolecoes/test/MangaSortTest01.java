b959d48449862ca3d587d13a66d37bc9fb175fa1
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-02-18 15:01:17 -0300

    feat: implementa interface Comparator para permitir ordenação customizada por ID

--- src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
+++ src/academy/devdojo/maratonajava/javacore/Ycolecoes/test/MangaSortTest01.java
@@ -4,7 +4,19 @@ import academy.devdojo.maratonajava.javacore.Ycolecoes.dominio.Manga;
 
 import java.util.ArrayList;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
+
+// Comparator -> Cria uma classe separada para cada critério de ordenação diferente (ex: ordenar por ID, por preço, por data).
+// Isso evita modificar a classe original toda vez que precisar de uma ordenação diferente.
+
+class MangaByIdComparator implements Comparator<Manga>{
+
+    @Override
+    public int compare(Manga manga1, Manga manga2) {
+        return manga1.getId().compareTo(manga2.getId()); // Usa a mesma logica de comparação do "Comparable".
+    }
+}
 
 public class MangaSortTest01 {
     public static void main(String[] args) {
@@ -17,10 +29,20 @@ public class MangaSortTest01 {
         mangas.add(new Manga(2L,"Pokemon", 3.2));
         mangas.add(new Manga(3L,"ATttack on titan", 11.20));
         mangas.add(new Manga(4L,"Dragon ball Z", 2.99));
+
         for (Manga manga : mangas) {
             System.out.println(manga);
         }
+
         Collections.sort(mangas);
+        System.out.println("---------------");
+        for (Manga manga : mangas) {
+            System.out.println(manga);
+        }
+
+        // Duas maneiras de como usar na Lista:
+//        Collections.sort(mangas, new MangaByIdComparator()); // Via Collections.
+        mangas.sort(new MangaByIdComparator()); // Via própria lista (Forma moderna).
         System.out.println("---------------");
         for (Manga manga : mangas) {
             System.out.println(manga);
