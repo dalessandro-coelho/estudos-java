54c2c4967b811e309d9c28143f47c1af4954114b
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-03-05 23:18:56 -0300

    docs: demonstra a duplicação de código em serviços específicos antes da aplicação de Generics

---
+++ src/academy/devdojo/maratonajava/javacore/Zgenerics/service/CarroRentavelService.java
@@ -0,0 +1,30 @@
+package academy.devdojo.maratonajava.javacore.Zgenerics.service;
+
+import academy.devdojo.maratonajava.javacore.Zgenerics.dominio.Carro;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class CarroRentavelService {
+
+    // É necessario a prática de criar classes genéricas para evitar a duplicação de
+    // código em situações onde a lógica é idêntica, mas os tipos de dados variam.
+
+    private List<Carro> carrosDisponiveis = new ArrayList<>(List.of(new Carro("BMW"), new Carro("Fusca")));
+
+    public Carro buscarCarroDisponivel(){
+        System.out.println("Buscando carro disponivel...");
+        Carro carro = carrosDisponiveis.remove(0);
+        System.out.println("Alugando carro: "+carro);
+        System.out.println("Carros Disponiveis para alugar");
+        System.out.println(carrosDisponiveis);
+        return carro;
+    }
+
+    public void retornarCarroAlugado(Carro carro){
+        System.out.println("Devolvendo carro"+carro);
+        carrosDisponiveis.add(carro);
+        System.out.println("Carros disponiveis para alugar: ");
+        System.out.println(carrosDisponiveis);
+    }
+}
