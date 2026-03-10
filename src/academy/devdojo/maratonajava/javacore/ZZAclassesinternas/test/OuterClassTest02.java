f1385d7abe1e3cdc9e6b9848cb300dd1ce2b105e
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-03-09 23:46:36 -0300

    feat: implementa classe local dentro de método e demonstra restrições de variáveis efetivamente finais

---
+++ src/academy/devdojo/maratonajava/javacore/ZZAclassesinternas/test/OuterClassTest02.java
@@ -0,0 +1,27 @@
+package academy.devdojo.maratonajava.javacore.ZZAclassesinternas.test;
+
+public class OuterClassTest02 {
+
+    // Classes locais -> São classes declaradas dentro de um metodo e só podem ser utilizadas dentro do escopo desse metodo.
+
+    private String name = "Midorya";
+
+    // Antes do java 8 era obrigado declarar "final", Agora basta que a variável seja "efetivamente final"(não seja modificada).
+    void print(final String param){
+        final String lastName = "Izuku";
+
+        // Não aceitam modificador de acesso "public" ou "private".
+        class LocalClass{
+            public void printLocal(){
+                System.out.println(param);
+                System.out.println(name + " "+ lastName);
+            }
+        }
+        // Classe Local inicializa dentro do metodo.
+        new LocalClass().printLocal();
+    }
+    public static void main(String[] args) {
+        OuterClassTest02 outer = new OuterClassTest02();
+        outer.print();
+    }
+}
