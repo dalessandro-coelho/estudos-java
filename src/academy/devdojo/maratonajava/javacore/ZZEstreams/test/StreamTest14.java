ff2ef6ed2d7487f44aa7c13c4ae8253ebc172a25
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-04-04 01:03:34 -0300

    feat: utiliza groupingBy com counting, maxBy e collectingAndThen

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest14.java
@@ -0,0 +1,49 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Category;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Promotion;
+
+import java.util.*;
+import java.util.function.BinaryOperator;
+import java.util.function.Function;
+import java.util.stream.Collectors;
+
+import static academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Promotion.NORMAL_PRICE;
+import static academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Promotion.UNDER_PROMOTION;
+import static java.util.stream.Collectors.groupingBy;
+
+public class StreamTest14 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)
+    ));
+    public static void main(String[] args) {
+
+        //Conta quantos itens existem em cada categoria (counting()).
+        Map<Category, Long> collect = lightNovels.stream().collect(groupingBy(LightNovel::getCategory, Collectors.counting()));
+        System.out.println(collect);
+
+        //Encontra o anime mais caro de cada categoria (Com Option).
+        Map<Category, Optional<LightNovel>> collect1 = lightNovels.stream()
+                .collect(groupingBy(LightNovel::getCategory, Collectors.maxBy(Comparator.comparing(LightNovel::getPrice))));
+        System.out.println(collect1);
+
+        //Encontra o anime mais caro de cada categoria, sem trabalhar com Optional(collectingAndThen()).
+        //collectingAndThen(): Permite executar uma ação logo após a conclusão de outro coletor.
+        Map<Category, LightNovel> collect2 = lightNovels.stream().collect(groupingBy(LightNovel::getCategory,
+                Collectors.collectingAndThen(Collectors.maxBy(Comparator.comparing(LightNovel::getPrice)), Optional::get)));
+        System.out.println(collect2);
+
+        //Encontra o anime mais caro de cada categoria, sem trabalhar com Optional (Outra forma).
+        Map<Category, LightNovel> collect3 = lightNovels.stream().collect(Collectors.toMap(LightNovel::getCategory,
+                Function.identity(), BinaryOperator.maxBy(Comparator.comparing(LightNovel::getPrice))));
+        System.out.println(collect3);
+     }
+}
