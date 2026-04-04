2cd29c2b1436d69f975175478987b9a9ac75406d
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-04-03 23:30:11 -0300

    feat: implementa agrupamento multinível e lógica personalizada com groupingBy

---
+++ src/academy/devdojo/maratonajava/javacore/ZZEstreams/test/StreamTest13.java
@@ -0,0 +1,46 @@
+package academy.devdojo.maratonajava.javacore.ZZEstreams.test;
+
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Category;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.LightNovel;
+import academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Promotion;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+import java.util.stream.Collectors;
+
+//Importação Estática (Reduz a verbosidade e torna a leitura da Stream muito mais limpa)
+import static academy.devdojo.maratonajava.javacore.ZZEstreams.dominio.Promotion.*;
+import static java.util.stream.Collectors.groupingBy;
+
+public class StreamTest13 {
+    private static List<LightNovel> lightNovels = new ArrayList<>(List.of(
+            new LightNovel("Tensei Shittara", 8.99, Category.FANTASY),
+            new LightNovel("Overlord", 10.99, Category.FANTASY),
+            new LightNovel("Violet Evergarden", 5.99, Category.DRAMA),
+            new LightNovel("No Game no life", 2.99, Category.FANTASY),
+            new LightNovel("Fullmetal Alchemist", 5.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Kumo desuga", 1.99, Category.FANTASY),
+            new LightNovel("Monogatari", 4.00, Category.ROMANCE)
+    ));
+    public static void main(String[] args) {
+
+        //Se o preço for menor que 6, o anime entra na chave UNDER_PROMOTION. Caso contrário, entra em NORMAL_PRICE.
+        Map<Promotion, List<LightNovel>> collect = lightNovels
+                .stream()
+                .collect(groupingBy(ln -> ln.getPrice() < 6 ? UNDER_PROMOTION : NORMAL_PRICE
+                ));
+        System.out.println(collect);
+
+        //Agrupamento Multinível (Nested Grouping): Vai agrupar e dentro de cada grupo, agrupa de novo.
+
+        //Os animes são agrupados primeiro por Categoria e, dentro de cada categoria, agrupados novamente por Promoção.
+        Map<Category, Map<Promotion, List<LightNovel>>> collect1 = lightNovels
+                .stream()
+                .collect(groupingBy(LightNovel::getCategory, groupingBy(ln -> ln.getPrice() < 6 ? UNDER_PROMOTION : NORMAL_PRICE
+                )));
+        System.out.println(collect1);
+    }
+}
