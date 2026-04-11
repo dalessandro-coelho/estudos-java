9e3e1df24a8cf8384a362f793a183143790fa13a
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-04-11 01:26:23 -0300

    feat: aplica sincronismo com a palavra-chave synchronized em métodos e blocos

--- src/academy/devdojo/maratonajava/javacore/ZZthreads/test/ThreadAccountTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZthreads/test/ThreadAccountTest01.java
@@ -2,8 +2,10 @@ package academy.devdojo.maratonajava.javacore.ZZthreads.test;
 
 import academy.devdojo.maratonajava.javacore.ZZthreads.dominio.Account;
 
-public class ThreadAccountTest01 implements Runnable{
-    private Account account = new Account();
+public class ThreadAccountTest01 implements Runnable {
+    //O objeto usado como Lock deve ser, preferencialmente, definido como "final".
+    private final Account account = new Account();
+
     public static void main(String[] args) {
 
         //Programação concorrente: a Race Condition (Condição de Corrida) -> O problema surge porque ambas as
@@ -19,25 +21,30 @@ public class ThreadAccountTest01 implements Runnable{
 
     @Override
     public void run() {
-        for (int i = 0; i < 5; i++){
+        for (int i = 0; i < 5; i++) {
             withdrawal(10);
-            if (account.getBalance() < 0){
+            if (account.getBalance() < 0) {
                 System.out.println("FODEO");
             }
         }
     }
 
-    private void withdrawal(int amount){
-        if(account.getBalance() >= amount){
-            System.out.println(getTheadName() + "está indo sacar dinheiro");
-            account.withdrawal(amount);
-            System.out.println(getTheadName() + "completou o saque, valor atual da conta " + account.getBalance());
-        } else {
-            System.out.println("Sem dinheiro para " + getTheadName() + "efetuar o saque " + account.getBalance());
+    //synchronized na assinatura do metodo garante que o metodo inteiro seja executado de forma atômica.
+    private /*synchronized*/ void withdrawal(int amount) {
+        System.out.println(getTheadName() + "#### fora do synchronized");
+        synchronized (account) { // Para não sincronizar o metodo todo, apenas um trecho especifico .
+            System.out.println(getTheadName() + "**** dentro do synchronized");
+            if (account.getBalance() >= amount) {
+                System.out.println(getTheadName() + " está indo sacar dinheiro");
+                account.withdrawal(amount);
+                System.out.println(getTheadName() + " completou o saque, valor atual da conta " + account.getBalance());
+            } else {
+                System.out.println("Sem dinheiro para " + getTheadName() + "efetuar o saque " + account.getBalance());
+            }
         }
     }
 
-    private String getTheadName(){
-        return  Thread.currentThread().getName();
+    private String getTheadName() {
+        return Thread.currentThread().getName();
     }
 }
