fd37204458c18f47172f7f267d36704fef2cca27
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-05-09 01:31:38 -0300

    chore: adiciona driver mysql e implementa ConnectionFactory

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/com/ConnectionFactory.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/com/ConnectionFactory.java
@@ -1,5 +1,23 @@
 package academy.devdojo.maratonajava.javacore.ZZIjdbc.com;
 
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+
 public class ConnectionFactory {
     //java.sql = Connection, Statement, ResultSet, DriverManager
+    public  static Connection getConnection(){
+        String url = "jdbc:mysql://localhost:3306/anime_store"; //URL de Conexão (Segue o padrão jdbc:mysql://[host]:[porta]/[nome_do_banco]).
+        String username = "root"; //O utilizador do banco
+        String password = "root"; //A senha definida na instalação ou no Docker
+
+        //É necessário tratar a SQLException, pois a conexão pode falhar (banco desligado, senha errada, etc.)
+        try {
+            Connection connection = DriverManager.getConnection(url, username, password);
+            System.out.println(connection);
+        } catch (SQLException e){
+            e.printStackTrace();
+        }
+        return null;
+    }
 }
