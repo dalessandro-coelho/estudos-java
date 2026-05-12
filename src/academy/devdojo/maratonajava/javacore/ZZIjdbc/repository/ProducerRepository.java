e87eb0f180632ddd90f5b62de126e5d5c80ad2a1
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-05-11 21:20:51 -0300

    chore: adiciona Lombok e Log4j2 para limpeza de código e logs profissionais

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -2,20 +2,20 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.repository;
 
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.com.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
+import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
 import java.sql.SQLException;
 import java.sql.Statement;
 
+@Log4j2
 public class ProducerRepository {
     public static void save(Producer producer) {
-        // String sql = "INSERT INTO producer (name) VALUES ('Madhouse');"; // Inserindo o nome Madhouse
         String sql = "INSERT INTO `anime_store`.`producer` (`name`) VALUES ('%s');".formatted(producer.getName()); // Inserindo outro nome
-        // Sempre que abre uma conexão (Connection) ou um mensageiro (Statement), precisas de fechá-los (Nesse caso usa "try-with-resources", que fecha tudo sozinho, mesmo que dê algum erro.).
         try (Connection conn = ConnectionFactory.getConnection();
-             Statement stmt = conn.createStatement()) { // Statement: Envia um comando SQL do Java para o MySQL. A conexão acontece usando a classe ConnectionFactory, que apartir dela gera o Statement.
-            int rowsAffected = stmt.executeUpdate(sql); // executeUpdate: Usado para comandos que alteram o banco (inserir, apagar ou atualizar). Ele retorna um número inteiro dizendo quantas linhas foram afetadas.
-            System.out.println(rowsAffected);
+             Statement stmt = conn.createStatement()) {
+            int rowsAffected = stmt.executeUpdate(sql);
+            log.info("Inserted producer in the Database, rows affected '{}'", rowsAffected);
         } catch (SQLException e) {
             e.printStackTrace();
         }
--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/test/ConnectionFactoryTest01.java
@@ -3,10 +3,24 @@ package academy.devdojo.maratonajava.javacore.ZZIjdbc.test;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.com.ConnectionFactory;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import academy.devdojo.maratonajava.javacore.ZZIjdbc.repository.ProducerRepository;
+import lombok.extern.log4j.Log4j2;
 
+@Log4j2
 public class ConnectionFactoryTest01 {
     public static void main(String[] args) {
-        Producer producer = Producer.ProducerBuilder.builder().name("NHK").build();
+        Producer producer = Producer.builder().name("Studio Deen").build();
         ProducerRepository.save(producer);
+
+        /*
+        * Log4j2: É um sistema de Logs profissional.
+        * Já que usar "System.out.println" acaba sendo peso, e não guarda
+        * informações importantes como a data, a hora ou o nível do erro num ficheiro.
+        * É possivel categorizar as mensagens usando "log":
+        * */
+//        log.info("info"); // INFO: Apenas avisando que algo aconteceu.
+//        log.debug("debug"); // DEBUG: Registra informações detalhadas sobre a execução.
+//        log.warn("warn"); // WARN: Indica situações inesperadas
+//        log.error("error"); // ERROR: Indica problemas sérios que impedem a execução
+//        log.trace("trace"); // TRACE: Nível mais detalhado de registro de eventos
     }
 }
