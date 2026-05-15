d2faef1930e208e7a15377ae59eef0f33b0eec24
Dalessandro Coelho <dalessandrojose3@gmail.com>
2026-05-14 22:21:50 -0300

    feat: implementa método findAll para listar todos os produtores usando ResultSet

--- src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
+++ src/academy/devdojo/maratonajava/javacore/ZZIjdbc/repository/ProducerRepository.java
@@ -5,8 +5,11 @@ import academy.devdojo.maratonajava.javacore.ZZIjdbc.dominio.Producer;
 import lombok.extern.log4j.Log4j2;
 
 import java.sql.Connection;
+import java.sql.ResultSet;
 import java.sql.SQLException;
 import java.sql.Statement;
+import java.util.ArrayList;
+import java.util.List;
 
 @Log4j2
 public class ProducerRepository {
@@ -44,5 +47,26 @@ public class ProducerRepository {
         } catch (SQLException e) {
             log.error("Error while trying to update producer '{}'", producer.getId(), e);
         }
+    }
+
+    public static List<Producer> findAll() {
+        log.info("Finding all Producers");
+        String sql = "SELECT id, name FROM anime_store.producer;"; //Comando para buscar os dados.
+        List<Producer> producers = new ArrayList<>();
+        try (Connection conn = ConnectionFactory.getConnection();
+             Statement stmt = conn.createStatement();
+             ResultSet rs = stmt.executeQuery(sql)) { //ResultSet: Armazena e acessa dados retornados de um banco de dados, após uma consulta SQL. / executeQuery: É obrigatorio usar para ler os dados.
+            while (rs.next()) { //next: faz duas coisas, pula para a próxima linha e diz "true" se houver dados lá, ou "false" se a tabela acabou.
+                Producer producer = Producer
+                        .builder()
+                        .id(rs.getInt("id")) //Pega o número da coluna ID.
+                        .name(rs.getString("name")) //Pega o texto da coluna Nome.
+                        .build();
+                producers.add(producer);
+            }
+        } catch (SQLException e) {
+            log.error("Error while trying to find all producer", e);
+        }
+        return producers;
     }
 }
